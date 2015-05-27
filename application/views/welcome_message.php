76a97305bdf12895c8a60df2aa555f99f3f85031
khadim-raath <khadim.raath@incubasys>
2015-05-27 15:02:09 +0500

    page fixed

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -20,20 +20,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <body>
 
         <div class="wrapper">
-            <h1>Welcome to Practice!</h1>
-            <div class="wrapper">
-                <form  action="<?= base_url(); ?>index.php/welcome/submit_servey" method="post">
+            <h1 class="title">Welcome to Practice!</h1>
+            <div class="container">
+                <div class="left-block">
+                    <img id="logo" src="" alt="Logo" />
+                </div>
+                <div class="right-block">
+                    <p>aaaaaaaaaaaaaaa</p>
+                    <p>aaaaaaaaaaaaaaa</p> 
+                    <p>aaaaaaaaaaaaaaa</p>
+                    <p>aaaaaaaaaaaaaaa</p>
+                    <p>aaaaaaaaaaaaaaa</p>
+                </div>
+                <form id="servey"  action="<?= base_url(); ?>index.php/welcome/submit_servey" method="post">
                     <div class="row form-group">
-                        <input class="form-control" type="text" name="ans1"/>
+                        <input class="form-control custom" type="text" name="ans1"/>
                     </div>
                     <div class="row form-group">
-                        <input class="form-control" type="text" name="ans2"/>
+                        <input class="form-control custom" type="text" name="ans2" data-trigger="change" data-parsley-required/>
                     </div>
                     <div class="row form-group">
-                        <input class="form-control" type="text" name="ans3"/>
+                        <input class="form-control custom" type="text" name="ans3"/>
                     </div>
                     <div class="row form-group">
-                        <input class="form-control" type="text" name="ans4"/>
+                        <input class="form-control custom" type="text" name="ans4"/>
                     </div>
 
                     <div class="row form-group">
